fecc205f3359531ebdec0ca6bc1d9a037e3efc0d
wumin <15371152260@163.com>
2022-11-11 21:47:31 +0800

    全局基础配置完成

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,26 @@ getTest()
 
 <template>
   <div>
-    我是app组件
+    <router-view />
+    <p>哈哈哈</p>
+    <ul>
+      <li>123</li>
+      <li>456</li>
+    </ul>
+    <div class="box">
+    我是 App
   </div>
+  </div>
+  
 </template>
 
 
 
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.box {
+  width: 300px;
+  height: 300px;
+  background-color:@xtxColor;
+  .hoverShadow()
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import 'normalize.css'
+
+import '@/assets/styles/common.less'
+
 createApp(App).mount('#app')
